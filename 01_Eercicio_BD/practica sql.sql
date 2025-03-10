2fe7ddd3e09df69b4430dcc0f6980d01d87fb386
Daniel250806 <danieljesus.diazdiaz2006@gmail.com>
2025-03-10 12:56:30 -0300

    wenaa

--- 01_Eercicio_BD/practica sql.sql
+++ 01_Eercicio_BD/practica sql.sql
@@ -4,6 +4,3 @@ use prueba;
 
 create table usuerio(id INT, username VARCHAR(50),
 password varchar(50));
-
-git config --global user.email "danieljesus.diazdiaz2006@gmail.com"
-git config --global user.name "Daniel250806"
